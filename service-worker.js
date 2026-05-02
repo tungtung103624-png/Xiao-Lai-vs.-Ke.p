a013177244f94e6f329e40877584ba7b252442c1
tungtung103624-png <tungtung103624@apps.ntpc.edu.tw>
2026-05-02 16:55:12 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,44 @@
-const CACHE_NAME = 'game-cache-v1';
+const CACHE_NAME = "xiao-lai-vs-ke-cache-v2";
+
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./app.js",
+  "./service-worker.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./1724774632_39142-removebg-preview.png",
+  "./ADEY0941-removebg-preview.png",
+  "./phpxNc89t-removebg-preview.png"
 ];
 
-// 安裝 Service Worker 並快取檔案
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
-// 攔截網路請求，優先從快取讀取
-self.addEventListener('fetch', event => {
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
