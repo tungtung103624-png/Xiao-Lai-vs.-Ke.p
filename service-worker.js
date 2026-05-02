37bdd3094f1cc9e37776130473961e9448b20bf3
tungtung103624-png <tungtung103624@apps.ntpc.edu.tw>
2026-05-02 15:38:34 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'game-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// 安裝 Service Worker 並快取檔案
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 攔截網路請求，優先從快取讀取
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
